e7ea73e93795e962cb6672ed6c295da191e9ac8f
3Sann <87173836+3Sann@users.noreply.github.com>
2025-03-15 22:34:47 +0800

    Update k_equal.m

--- k_equal.m
+++ k_equal.m
@@ -1,6 +1,12 @@
-function Kequal = k_equal(k)
-    statusInFJ = nchoosek(2+k,2)*nchoosek(2+k,2) - nchoosek(2+k-1,1)*nchoosek(2+k-1,2);
-    M = 4;
+function Kequal = k_equal(k,n)
+    statusLeft = 1;
+    statusRight = 1;
+    for i=1:n
+        statusLeft = statusLeft*nchoosek(2+k,2);
+        statusRight = statusRight*nchoosek(2+k-1,2);
+    end
+    statusInFJ = statusLeft - statusRight;
+    M = 2*n;
     statusAboutk = nchoosek(M - 1 + 1, M - 1);
     mindis = abs(statusInFJ - statusAboutk);
     Kequal = 1;
@@ -11,7 +17,7 @@ function Kequal = k_equal(k)
             break
         end
         Kequal = i;
-        if (Kequal - k)/k > 0.3
+        if (Kequal - k)/k > 0.2       
             break
         end
         mindis = dis;
